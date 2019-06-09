536fd5f47733ffbe545040af45276c0258063fb7
Nikil Kumar <kumarnikil9865@gmail.com>
2019-06-09 23:39:28 +0800

    added actual video script

---
+++ video_recog.py
@@ -0,0 +1,77 @@
+import cv2
+import face_recognition as fr
+import os
+import sys
+import numpy
+from matplotlib import pyplot as plt
+import time
+
+db_path = '/Users/NickKumar/EE_training/face_db'
+file_names = os.listdir(db_path)
+
+# Creating a list of the names/titles of known faces (array known_face_names)
+known_faces_names = list()
+
+# Creating a list to store the face encodings of the known faces after computing the face encodings
+known_face_encodings = list()
+
+for file_name in file_names:
+    # Adding unedited names to list of face names.
+    if (file_name == '.DS_Store' or file_name == '.DS_STORE'):
+        continue
+    known_faces_names.append(file_name.split('.')[0])
+
+    # Adding face encodings to detected faces
+    name = os.path.join(db_path, file_name)
+    image = fr.api.load_image_file(name, mode='RGB')
+    face_encoding = fr.api.face_encodings(image)[0]
+    known_face_encodings.append(face_encoding)
+
+print(known_faces_names)
+
+# Turn on Video Camera / Web Cam
+vs = cv2.VideoCapture(0)
+time.sleep(2)
+
+while True:
+    face, frame = vs.read()
+    small_frame = cv2.resize(frame, (0,0), fx=1/4, fy=1/4)
+    
+    # Create a list of face encodings from each frame
+    face_locations = fr.api.face_locations(small_frame)
+    frame_face_encodings = fr.api.face_encodings(small_frame)
+
+    print(len(face_locations))
+    print('')
+    print(len(frame_face_encodings))
+
+    for i, frame_face_encoding in enumerate(frame_face_encodings):
+        matches = fr.api.compare_faces(known_face_encodings, frame_face_encoding, tolerance = 0.6) # Return a list of matches faces if True.
+
+        if True in matches:
+            index = matches.index(True)
+            name = known_faces_names[index] 
+        else:
+            name = "Unknown Person"
+
+    # For each face detected in given frame, compare the faces with the encodings of known faces to check if same person.
+    top, right, bottom, left = face_locations[i]
+    top *= 4
+    right *= 4
+    bottom *= 4
+    left *= 4
+    cv2.rectangle(frame, (left,top), (right,bottom), (0, 255, 0), 2,cv2.LINE_AA)
+    cv2.putText(frame, name, (left , bottom + 30), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 255, 0), 3)
+    
+    cv2.imshow('Frame', frame)
+
+    if frame is None:
+        print('Frame is none.')
+        break
+    
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
